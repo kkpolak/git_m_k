7868d72649c07a6d80d4095f735f0a52d5da45b1
Imię Nazwisko <twoj@email.com>
2026-02-13 16:27:07 +0100

    feat: add pasta to menu

--- src/main/java/com/example/Menu.java
+++ src/main/java/com/example/Menu.java
@@ -4,6 +4,7 @@ public class Menu {
     public static void main(String[] args) {
         System.out.println("=== MENU RESTAURACJI ===");
         System.out.println("1. Pizza Margherita");
+        System.out.println("1. Pizza Margherita 2");
         System.out.println("2. Burger klasyczny");
         System.out.println("3. Pasta Carbonara");
     }
